15b076ccba45491ac70a1d6fb7ae3dd53905e386
Administrador1 <administrador1@SeverPreviAsist1.previasist-domain1>
2019-04-11 13:04:35 -0400

    correccion de sql de instalacion

--- sql/mysql/install.mysql.utf8.sql
+++ sql/mysql/install.mysql.utf8.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `#__mod_mymodule` (
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-INSERT INTO `#__mod_mymodule` (`campo`, `content`, `extra`, `posit`, `present`) VALUES ('Fondo', 'images/imagenfondo.png', '[{}]',  0, true)
+INSERT INTO `#__mod_mymodule` (`campo`, `content`, `extra`, `posit`, `present`) VALUES ('Fondo', 'images/imagenfondo.png', '[{}]',  0, true);
 INSERT INTO `#__mod_mymodule` (`campo`, `content`, `extra`, `posit`, `present`) VALUES ('Titulo', 'Welcome To Our Studio!', '[{}]', 1, true);
 INSERT INTO `#__mod_mymodule` (`campo`, `content`, `extra`, `posit`, `present`) VALUES ('Subtitulo', 'Its Nice To Meet You', '[{}]', 2, true);
 INSERT INTO `#__mod_mymodule` (`campo`, `content`, `extra`, `posit`, `present`) VALUES ('Boton', 'Saber mas', '[{ "linkButom":"#" }]',  3, true);
